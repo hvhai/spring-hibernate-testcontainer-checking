07a5e4c0d87a417cbabfd27aa38d6d0f6f48a8a2
hvhai <hunterx6323@gmail.com>
2023-08-03 16:07:53 +0700

    refactor: split to TestContainerBaseTest

---
+++ src/test/java/com/codehunter/springhibernatetestcontainerchecking/TestContainerBase.java
@@ -0,0 +1,22 @@
+package com.codehunter.springhibernatetestcontainerchecking;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public class TestContainerBase {
+    @Container
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.33");
+
+    @DynamicPropertySource
+    static void mySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+}
