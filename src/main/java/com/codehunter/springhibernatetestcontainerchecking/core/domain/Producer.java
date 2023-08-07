fea5253a70932a2de63cac729c7c8328b9f5f212
hvhai <hunterx6323@gmail.com>
2023-08-07 16:13:00 +0700

    fix: update model

--- src/main/java/com/codehunter/springhibernatetestcontainerchecking/core/domain/Producer.java
+++ src/main/java/com/codehunter/springhibernatetestcontainerchecking/core/domain/Producer.java
@@ -1,7 +1,9 @@
 package com.codehunter.springhibernatetestcontainerchecking.core.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -9,6 +11,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Producer {
     private Long id;
     private String name;
